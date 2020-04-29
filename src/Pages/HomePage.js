e37f5da61a78d6721afa3c8edd19d46111eb43fa
Martin Spicuzza <spicuzza157@gmail.com>
2020-04-29 14:20:14 -0500

    adding quick styling elements for grid elements

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -3,15 +3,12 @@ import React from "react";
 const HomePage = (props) => {
   return (
     <div className="page-area" id="home-page">
-
-<div className="jumbotron neu-container">
+      <div className="jumbotron neu-container">
         <div>Hello!</div>
       </div>
-      
+
       <div className="grid d-1x2">
-        <div className="grid-item neu-container">
-          Home Area 1
-        </div>
+        <div className="grid-item neu-container">Home Area 1</div>
 
         <div className="grid-item grid d-2x2 w-50 neu-container">
           <div className="grid-item neu-container inset">Space 1</div>
@@ -19,6 +16,12 @@ const HomePage = (props) => {
           <div className="grid-item neu-container inset">Space 3</div>
           <div className="grid-item neu-container inset">Space 4</div>
         </div>
+      </div>
+
+      <div className="grid d-1x3">
+          <div className="grid-item neu-container">Space 1</div>
+          <div className="grid-item neu-container">Space 2</div>
+          <div className="grid-item neu-container">Space 3</div>
       </div>
     </div>
   );
