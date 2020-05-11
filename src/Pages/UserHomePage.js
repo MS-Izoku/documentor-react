0430a5c9d482bde0be5a44d84850071eeee72110
Martin Spicuzza <spicuzza157@gmail.com>
2020-05-10 23:59:35 -0500

    new stuff on the userhomepage

--- src/Pages/UserHomePage.js
+++ src/Pages/UserHomePage.js
@@ -2,10 +2,37 @@ import React, {Component} from 'react'
 import {withRouter} from 'react-router'
 
 class UserHomePage extends Component{
+    constructor(){
+        super()
+        this.state={
+            projects: []
+        }
+    }
+
+    renderProjects = () =>{
+        this.state.projects.map(project => <div className="neu-container p-2">
+            <h3>{project.name}</h3>
+            <div className="neu-container inverse">
+                <div id={project.name + likes + id}>
+                    Likes: {project.likeCount}
+                </div>
+                <div id={project.name + projects.bookCount + project.id}>
+                    {project.bookCount}
+                </div>
+            </div>
+        </div>)
+    }
+
     render(){
         return <div className="page-area">
             <div id="user-landing-jumbotron" className="jumbotron">
                 User Landing Jumbotron
+            </div>
+            <div className="wrapper col">
+                <h2>Recent Projects</h2>
+                <div className="wrapper">
+                    {this.renderProjects()}
+                </div>
             </div>
             <div className="row">
                 <div className="col neu-container">Section 1</div>
